7c13205d267b45bc86d7056d3502187c861ea929
hoangnorpscripter <152389580+hoangnorpscripter@users.noreply.github.com>
2024-05-08 13:21:20 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,4 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/hoangnorpscripter/nguyenanhthu/main/keysystem.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/hoangnorpscripter/nguyenanhthu/main/key2315123.lua"))()
+-- waiting for check key
+wait(5)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/hoangnorpscripter/scriptbykhanh/main/Choosehubchiyeuminhthuw.lua"))()
